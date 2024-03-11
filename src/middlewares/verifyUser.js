7528b2781068d2f05a285c73fa76f848c5e20ca2
tjd985 <tjd985@gmail.com>
2024-03-12 06:41:12 +0900

    ✨ [Feat] verifyUser 구현
    verifyUser 미들웨어 구현

---
+++ src/middlewares/verifyUser.js
@@ -0,0 +1,47 @@
+const { isValidObjectId } = require("mongoose");
+const createError = require("http-errors");
+
+const TemporaryUser = require("../models/TemporaryUser");
+
+const ERROR = require("../constants/error");
+
+async function verifyUser(req, res, next) {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    res.json({
+      result: "error",
+      status: ERROR.BAD_REQUEST.status,
+      message: ERROR.BAD_REQUEST.message,
+    });
+
+    return;
+  }
+
+  try {
+    const temporaryUser = await TemporaryUser.findById(id);
+
+    if (!temporaryUser) {
+      res.json({
+        result: "error",
+        status: ERROR.NOT_FOUND.status,
+        message: ERROR.NOT_FOUND.message,
+      });
+
+      return;
+    }
+
+    res.locals.temporaryUser = temporaryUser;
+
+    next();
+  } catch (err) {
+    const customError = createError(
+      ERROR.SERVER_ERROR.status,
+      ERROR.SERVER_ERROR.message,
+    );
+
+    next(customError);
+  }
+}
+
+module.exports = verifyUser;

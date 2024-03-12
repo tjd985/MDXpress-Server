13192f67387b1bf6de6ebd6b9361e049e1840295
tjd985 <tjd985@gmail.com>
2024-03-13 02:16:29 +0900

    ♻️ [Refactor] 스키마 구조 수정
    Version 스키마 구조 수정

--- src/models/Version.js
+++ src/models/Version.js
@@ -1,20 +1,15 @@
 const mongoose = require("mongoose");
 
-const deleteFromParent = require("../middlewares/deleteFromParent");
 const CONSTANTS = require("../constants/constants");
 
 const VersionSchema = new mongoose.Schema({
-  parendId: { type: mongoose.Types.ObjectId, require: true },
   version: { type: Number, require: true, default: 0 },
   code: { type: String, require: true, default: "" },
-  createdAt: {
+  expireAt: {
     type: Date,
-    required: true,
     expires: CONSTANTS.EXPIRED_SECOND,
-    default: Date.now(),
+    default: Date.now,
   },
 });
-
-VersionSchema.pre("remove", deleteFromParent);
 
 module.exports = mongoose.model("Version", VersionSchema);

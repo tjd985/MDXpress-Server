a96eb347e9d9bb82ad0b279928d686f369413353
tjd985 <tjd985@gmail.com>
2024-03-13 04:23:58 +0900

    ♻️ [Refactor] 모듈로 분리
    changeStream 모듈함수로 분리

---
+++ src/utils/changeStream.js
@@ -0,0 +1,19 @@
+const TemporaryUser = require("../models/TemporaryUser");
+const Version = require("../models/Version");
+
+Version.watch().on("change", async (changedData) => {
+  if (changedData.operationType === "delete") {
+    const allUsers = await TemporaryUser.find();
+    const deletedVersionId = changedData.documentKey._id.toString();
+
+    for (const user of allUsers) {
+      user.versions.forEach((versionId, index) => {
+        if (versionId.toString() === deletedVersionId) {
+          user.versions.splice(index, 1);
+        }
+      });
+
+      await user.save();
+    }
+  }
+});

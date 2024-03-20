614b7e42924b9da19bba4c2fbd33c3385aa8f4ae
tjd985 <tjd985@gmail.com>
2024-03-20 22:02:44 +0900

    ♻️ [Refactor] 스키마 수정
    packageList 필드 추가

--- src/models/Version.js
+++ src/models/Version.js
@@ -5,6 +5,7 @@ const CONSTANTS = require("../constants/constants");
 const VersionSchema = new mongoose.Schema({
   version: { type: Number, require: true, default: 0 },
   code: { type: String, require: true, default: "" },
+  packageList: { type: Object, require: true, default: {} },
   expireAt: {
     type: Date,
     expires: CONSTANTS.EXPIRED_SECOND,

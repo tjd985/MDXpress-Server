b8c10939bebab0f2bc75a7048287e66ee68da29e
tjd985 <tjd985@gmail.com>
2024-03-21 14:42:46 +0900

    ✨ [Feat] 도커 로더 추가
    도커 로더 추가

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,10 +1,12 @@
 const expressLoader = require("./express");
 const mongooseLoader = require("./mongoose");
 const routerLoader = require("./routers");
+const dockerLoader = require("./docker");
 const errorLoader = require("./error");
 
 const appLoader = async (app) => {
   await mongooseLoader();
+  await dockerLoader();
   expressLoader(app);
   routerLoader(app);
   errorLoader(app);

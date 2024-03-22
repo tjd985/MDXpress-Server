83e74856051e3416485e3ec16a6bd0072d6e1d46
tjd985 <tjd985@gmail.com>
2024-03-22 15:18:18 +0900

    🐛 [Fix] 도커 이미지 로드 에러핸들링
    도커 이미지로드중 에러 발생시 서버 종료 버그 대응

--- src/loaders/docker.js
+++ src/loaders/docker.js
@@ -2,7 +2,13 @@ const util = require("util");
 const exec = util.promisify(require("node:child_process").exec);
 
 async function getDockerImage() {
-  await exec("docker pull tjd985/mdxpress-docker:1.0");
+  try {
+    await exec("docker pull tjd985/mdxpress-docker:1.0");
+
+    console.log("Docker image loaded successfully");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = getDockerImage;

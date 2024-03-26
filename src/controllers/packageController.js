87c7b0fba40c6134d088454ca504eadb8e7ba641
tjd985 <tjd985@gmail.com>
2024-03-26 15:03:12 +0900

    [Style] 에러핸들링 로그 추가

--- src/controllers/packageController.js
+++ src/controllers/packageController.js
@@ -12,12 +12,10 @@ async function getBundledPackageCode(req, res, next) {
     const dockerCommand = `docker run --rm tjd985/mdxpress-docker:latest ${packageName} sh -c cat /${packageName}.js`;
 
     const { stdout: commandResult, stderr } = await exec(dockerCommand);
-    const newLineIndex = commandResult.indexOf("\n");
-
-    const packageInformation = commandResult.slice(0, newLineIndex);
-    const bundledPackageCode = commandResult.slice(newLineIndex);
 
     if (stderr) {
+      console.error("stderr:", stderr);
+
       res.json({
         result: "Error",
         status: ERROR_PATTERNS.BAD_REQUEST.status,
@@ -26,6 +24,10 @@ async function getBundledPackageCode(req, res, next) {
 
       return;
     }
+    const newLineIndex = commandResult.indexOf("\n");
+
+    const packageInformation = commandResult.slice(0, newLineIndex);
+    const bundledPackageCode = commandResult.slice(newLineIndex);
 
     res.json({
       result: "OK",
@@ -36,6 +38,8 @@ async function getBundledPackageCode(req, res, next) {
       },
     });
   } catch (err) {
+    console.error("exec error:", err);
+
     const customError = createError(
       ERROR.SERVER_ERROR.status,
       ERROR.SERVER_ERROR.message,
